8a6fc9ed3eef068e013c118185ff2525d7fd895e
JosefinBack <josefin.m.mansson@gmail.com>
2025-06-08 19:12:31 +0200

    Update header.js

--- Javascript/header.js
+++ Javascript/header.js
@@ -5,7 +5,7 @@ document.getElementById("header").innerHTML = `
             <a class="menyButton" href="projekt1.html">Projekt 1</a>
             <a class="menyButton" href="projekt2.html">Projekt 2</a>
             <a class="menyButton" href="projekt3.html">Projekt 3</a>
-            <a class="menyButton" href="HTML/memory.html">Memory</a>
+            <a class="menyButton" href="../HTML/memory.html">Memory</a>
             <a class="menyButton" href="about.html">Om mig</a>
         </div>
     </header>
