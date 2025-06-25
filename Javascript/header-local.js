eadb69026bee3d612473ff5a76a4bcf0409e71b5
JosefinBack <josefin.m.mansson@gmail.com>
2025-06-25 14:32:59 +0200

    mobinanpassning fixad

--- Javascript/header-local.js
+++ Javascript/header-local.js
@@ -1,12 +1,24 @@
 document.getElementById("header").innerHTML = `
   <header id="headerFront">
-    <div id="topMenu">
-      <a class="menyButton" href="../index.html">Hem</a>
-      <a class="menyButton" href="projekt1.html">Projekt 1</a>
-      <a class="menyButton" href="projekt2.html">Projekt 2</a>
-      <a class="menyButton" href="projekt3.html">Projekt 3</a>
-      <a class="menyButton" href="memory.html">Memory</a>
-      <a class="menyButton" href="about.html">Om mig</a>
-    </div>
+    <div id="menuToggle">MENY</div>
+    <nav id="topMenu">
+      <a class="menyButton" href="index.html">Hem</a>
+      <a class="menyButton" href="HTML/projekt1.html">Projekt 1</a>
+      <a class="menyButton" href="HTML/projekt2.html">Projekt 2</a>
+      <a class="menyButton" href="HTML/projekt3.html">Projekt 3</a>
+      <a class="menyButton" href="HTML/memory.html">Memory</a>
+      <a class="menyButton" href="HTML/about.html">Om mig</a>
+    </nav>
   </header>
 `;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const menu = document.getElementById("topMenu");
+
+  if (menuToggle && menu) {
+    menuToggle.addEventListener("click", function () {
+      menu.classList.toggle("show");
+    });
+  }
+});
