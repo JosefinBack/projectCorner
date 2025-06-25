1391d222ecf19127390324070a2f619b932b422e
JosefinBack <josefin.m.mansson@gmail.com>
2025-06-25 14:34:24 +0200

    Update header.js

--- Javascript/header.js
+++ Javascript/header.js
@@ -1,13 +1,26 @@
 document.getElementById("header").innerHTML = `
   <header id="headerFront">
-    <div id="topMenu">
+    <div id="menuToggle">MENY</div>
+    <nav id="topMenu">
       <a class="menyButton" href="/projectCorner/index.html">Hem</a>
       <a class="menyButton" href="/projectCorner/HTML/projekt1.html">Projekt 1</a>
       <a class="menyButton" href="/projectCorner/HTML/projekt2.html">Projekt 2</a>
       <a class="menyButton" href="/projectCorner/HTML/projekt3.html">Projekt 3</a>
       <a class="menyButton" href="/projectCorner/HTML/memory.html">Memory</a>
       <a class="menyButton" href="/projectCorner/HTML/about.html">Om mig</a>
-    </div>
+    </nav>
   </header>
 `;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menuToggle");
+  const menu = document.getElementById("topMenu");
+
+  if (toggle && menu) {
+    toggle.addEventListener("click", function () {
+      menu.classList.toggle("show");
+    });
+  }
+});
+
 
