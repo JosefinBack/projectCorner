2bf9f25d642d98c764e8842ea9cab90eb8e2f85a
JosefinBack <josefin.m.mansson@gmail.com>
2025-09-20 14:54:14 +0200

    close book

--- Javascript/books.js
+++ Javascript/books.js
@@ -1,13 +1,44 @@
 
-const addBook = document.getElementById("addBoook");
+let addBook = document.getElementById("addBook");
+let closeBook = document.getElementById("close");
+let starContainers = document.getElementsByClassName("stars");
+let createBook = document.getElementById("createBook");
 
+
+//functions
+function createABook() {
+    createBook.style.display = "block";
+}
+
+function closeCreateook() {
+    createBook.style.display = "none";
+}
 
 addBook.addEventListener("click", function () {
-    createBook();
+    createABook();
 });
 
+closeBook.addEventListener("click", function () {
+    closeCreateook();
+})
 
-function createBook() {
+//färga stjärnorna 
+for (let i = 0; i < starContainers.length; i++) {
+    let container = starContainers[i];
+    let stars = container.getElementsByTagName("span");
 
+    // gör varje stjärna klickbar
+    for (let j = 0; j < stars.length; j++) {
+        stars[j].addEventListener("click", function () {
+
+            for (let k = 0; k < stars.length; k++) {
+                if (k <= j) {
+                    stars[k].classList.add("filled");
+                } else {
+                    stars[k].classList.remove("filled");
+                }
+            }
+        });
+    }
 }
 
