b0c77b15fc6c562472763577753a234588d86f87
JosefinBack <josefin.m.mansson@gmail.com>
2025-11-27 16:26:26 +0100

    calender start

--- Javascript/calender.js
+++ Javascript/calender.js
@@ -19,12 +19,13 @@
 // console.log(array);
 
 
-
+let main = document.querySelector("main");
 
 const calender = document.getElementById("calender");
-const monthNameEl = document.getElementById("monthName");
+const monthName = document.getElementById("monthName");
 const btnPrev = document.getElementById("prevMonth");
 const btnNext = document.getElementById("nextMonth");
+const dateOfToday = document.getElementById("dateToday");
 
 const months = [
     "Januari", "Februari", "Mars", "April", "Maj", "Juni",
@@ -35,8 +36,8 @@ let currentYear = new Date().getFullYear();
 let currentMonth = new Date().getMonth();
 
 function renderCalendar(year, monthIndex) {
-    calender.innerHTML = ""; // töm kalendern
-    monthNameEl.textContent = `${months[monthIndex]} ${year}`;
+    calender.innerHTML = "";
+    monthName.textContent = `${months[monthIndex]} ${year}`;
 
     // Hämta antal dagar i månaden
     const daysInMonth = new Date(year, monthIndex + 1, 0).getDate();
@@ -45,11 +46,11 @@ function renderCalendar(year, monthIndex) {
     let firstDay = new Date(year, monthIndex, 1).getDay();
     if (firstDay === 0) firstDay = 7; // gör söndag till 7
 
+
     // Skapa tomma rutor innan första dagen (för att rätta uppstart)
     for (let i = 1; i < firstDay; i++) {
         const empty = document.createElement("div");
-        empty.classList.add("day");
-        empty.style.border = "none";
+        empty.style.border = "1px solid lightgrey";
         calender.appendChild(empty);
     }
 
@@ -60,10 +61,39 @@ function renderCalendar(year, monthIndex) {
         dayDiv.textContent = i;
         calender.appendChild(dayDiv);
     }
+
+    let lastDay = new Date(year, monthIndex + 1, 0).getDay();
+    if (lastDay === 0) lastDay = 7; // söndag = 7
+    let emptyBoxes = 7 - lastDay;
+
+    for (let i = 0; i < emptyBoxes; i++) {
+        const empty = document.createElement("div");
+        empty.style.border = "1px solid lightgrey";
+        calender.appendChild(empty);
+    }
+
+    let today = new Date().getDate();
+    allDays = document.querySelectorAll(".day");
+    allDaysArray = Array.from(allDays);
+
+    todayMonth = new Date().getMonth() + 1
+
+    dateOfToday.innerHTML = `
+    Dagens datum: ${currentYear} - ${todayMonth} - ${today}`
+    dateOfToday.style.fontSize = "20px"
+
+    for (let i = 1; i < daysInMonth; i++) {
+        if (i == today) {
+            rightDay = allDaysArray.find(D => D.textContent == i);
+            rightDay.classList.remove("day")
+            rightDay.classList.add("rightDay")
+        }
+    }
 }
 
+
 // Byt månad framåt
-btnNext.addEventListener("click", () => {
+btnNext.addEventListener("click", function () {
     currentMonth++;
     if (currentMonth > 11) {
         currentMonth = 0;
@@ -73,7 +103,7 @@ btnNext.addEventListener("click", () => {
 });
 
 // Byt månad bakåt
-btnPrev.addEventListener("click", () => {
+btnPrev.addEventListener("click", function () {
     currentMonth--;
     if (currentMonth < 0) {
         currentMonth = 11;
@@ -85,3 +115,10 @@ btnPrev.addEventListener("click", () => {
 // Visa aktuell månad vid start
 renderCalendar(currentYear, currentMonth);
 
+
+let edit = document.getElementById("edit");
+
+
+edit.addEventListener("click", function () {
+    window.location.href = "calenderEdit.html"
+})
---
+++ Javascript/calenderEdit.js
@@ -0,0 +1,8 @@
+
+let backToCalender = document.getElementById("backButton");
+
+
+
+backToCalender.addEventListener("click", function () {
+    window.location.href = "calender.html"
+})
